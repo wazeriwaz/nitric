22846b48e144142c6ebacdeccaa20be088da8971
Tim Holm <tim.holm@nitric.io>
2024-05-10 10:25:26 +1000

    add simple readiness probe.

--- cloud/azure/deploy/service.go
+++ cloud/azure/deploy/service.go
@@ -362,6 +362,15 @@ func (p *NitricAzurePulumiProvider) Service(ctx *pulumi.Context, parent pulumi.R
 						Memory: pulumi.Sprintf("%.2fGi", serviceConfig.ContainerApps.Memory),
 					},
 					Env: env,
+					Probes: app.ContainerAppProbeArray{
+						app.ContainerAppProbeArgs{
+							TcpSocket: app.ContainerAppProbeTcpSocketArgs{
+								Port: pulumi.Int(9001),
+							},
+							TimeoutSeconds: pulumi.Int(5),
+							Type:           pulumi.String("readiness"),
+						},
+					},
 				},
 			},
 		},
